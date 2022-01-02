9478bf1b085d3d287a090eb3d2cdb0ebdc9dd170
Ricardo Mendez <rmendez13@miners.utep.edu>
2022-01-01 22:57:49 -0700

    finished grid solution, Happy New Year

--- Grid Memoization/GridSolution.py
+++ Grid Memoization/GridSolution.py
@@ -1,7 +1,12 @@
 class Solution:
-    def gridTraveler(m, n):
+    def gridTraveler(m, n, memo = {}):
+        key = m + ',' + n
+        if key in memo:
+            return memo[key]
         if( m == 1 & n ==1):
             return 1
         if(m == 0 & n == 0):
             return 1
-        return Solution.gridTraveler(m - 1, n) + Solution.gridTraveler(m, n -1);
+        
+        memo[key] = Solution.gridTraveler(m - 1, n) + Solution.gridTraveler(m, n-1)
+        return memo[key]
