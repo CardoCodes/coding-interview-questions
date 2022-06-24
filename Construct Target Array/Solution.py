4648ca09faa55958cbdf791dbf819c164f872792
Ricardo Mendez <78371077+CardoCodes@users.noreply.github.com>
2022-06-24 14:06:28 -0600

    comments

--- Construct Target Array/Solution.py
+++ Construct Target Array/Solution.py
@@ -3,14 +3,15 @@ class Solution:
         # 1,1 -> 1,2 -> 3,2 -> 3,5 -> 8,5
         # 1*n will always be first digit 
         
-        heap = [-num for num in target]
-        total = sum(target)
+        heap = [-num for num in target] #create heap from target list
+        total = sum(target) #total total is sum of target 
         heapify(heap)
         
         while heap[0] != -1:
             num = -heappop(heap)
             total -= num
-            if num <= total or total < 1 : return False
+            if num <= total or total < 1 : return False # false if -heappop(num) is less than or equal to total 
+            
             num %= total
             total += num
             heappush(heap, -num or -total)
